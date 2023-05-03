ecf82f3fde85de59466a9137b35b5356eab08b6f
Yota Adachi <yota961219@gmail.com>
2023-05-03 13:54:14 +0900

    4日目

---
+++ 4日目/null1.sql
@@ -0,0 +1,11 @@
+-- IN + NULL
+SELECT * FROM customers WHERE name IS NULL;
+SELECT * FROM customers WHERE name IN ("河野 文典", "稲田 季雄") OR name IS NULL;
+
+-- NOT IN name != "河野 文典" name != "稲田 季雄" name != NULL
+SELECT * FROM customers WHERE name NOT IN("河野 文典", "稲田 季雄", NULL);
+SELECT * FROM customers WHERE name NOT IN("河野 文典", "稲田 季雄") AND name IS NOT NULL;
+
+-- ALL
+-- customersテーブルからid < 10の人の誕生日よりも古い誕生日の人をusersから取り出すSQL
+SELECT * FROM users WHERE birth_day <= ALL(SELECT birth_day FROM customers WHERE id < 10 AND birth_day IS NOT NULL);
