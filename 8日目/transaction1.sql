bfb3ff69d356fec0f100d58c53f0deaba5d01dbf
Yota Adachi <yota961219@gmail.com>
2023-05-07 15:18:48 +0900

    8日目

---
+++ 8日目/transaction1.sql
@@ -0,0 +1,21 @@
+SHOW DATABASES;
+
+USE day_4_9_db;
+
+SHOW TABLES;
+
+SELECT * FROM users;
+
+-- TRANSACTIONの開始
+START TRANSACTION;
+
+-- UPDATE処理
+UPDATE users SET name = "奥山 成美" WHERE id = 1;
+
+SELECT * FROM users;
+
+-- ROLLBACK
+ROLLBACK;
+
+-- COMMIT(トランザクションをDBに反映)
+COMMIT;
