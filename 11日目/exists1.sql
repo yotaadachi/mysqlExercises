23afbbf8ea410e191c6b97c0fa9b3ed6efb0dec2
Yota Adachi <yota961219@gmail.com>
2023-07-15 15:23:51 +0900

    11日目

--- 11日目/exists1.sql
+++ 11日目/exists1.sql
@@ -25,5 +25,8 @@ WHERE EXISTS(
  SELECT 1 FROM departments AS dt WHERE dt.name IN ("営業部", "開発部") AND em.department_id = dt.id
 );
 
+SELECT * FROM customers AS ct
+WHERE EXISTS(SELECT * FROM orders AS od WHERE ct.id = od.customer_id AND od.order_date = "2020-12-31");
 
-
+SELECT * FROM employees AS em1
+WHERE NOT EXISTS(SELECT 1 FROM employees AS em2 WHERE em2.manager_id = em1.id);
