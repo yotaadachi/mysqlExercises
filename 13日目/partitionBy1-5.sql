c5346d483bae6e2efc27578d4931daed42631568
Yota Adachi <yota961219@gmail.com>
2023-07-22 10:02:29 +0900

    13日目

--- 13日目/partitionBy1-5.sql
+++ 13日目/partitionBy1-5.sql
@@ -26,3 +26,15 @@ FROM employees;
 
 SELECT *, SUM(order_price) OVER(ORDER BY order_date, customer_id)
 FROM orders;
+
+-- PARTITION BY + ORDER BY
+SELECT *,
+COUNT(*) OVER(PARTITION BY department_id ORDER BY age) AS count_value
+FROM employees;
+
+-- 月ごとの、合計をemployeesのIDで昇順に並び替えて出す
+SELECT *,
+MAX(payment) OVER(PARTITION BY sa.paid_date ORDER BY emp.id)
+FROM employees AS emp
+INNER JOIN salaries AS sa
+ON emp.id = sa.employee_id;
