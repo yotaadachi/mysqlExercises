30adb29ea5e910f1ee54c29832ab47f063f8992d
Yota Adachi <yota961219@gmail.com>
2024-02-17 16:21:04 +0900

    10日目

--- 10日目/subquery1.sql
+++ 10日目/subquery1.sql
@@ -80,3 +80,29 @@ FROM
  customers AS cs
 WHERE
  cs.id < 10;
+ 
+-- CASEと使用（副問い合わせ）
+SELECT
+ emp.*, 
+ CASE
+  WHEN emp.department_id = (
+   SELECT id FROM departments WHERE name = "経営企画部"
+  )
+  THEN "経営層"
+  ELSE "その他"
+ END AS "役割"
+FROM
+ employees AS emp;
+
+SELECT
+ emp.*,
+ CASE
+ 	WHEN emp.id IN(
+ 	SELECT DISTINCT employee_id FROM salaries WHERE payment > (
+     SELECT AVG(payment) FROM salaries
+     )
+ 	) THEN "○"
+ 	ELSE "×"
+ END　AS "平均より給料が高いか"
+FROM
+　employees AS emp;
