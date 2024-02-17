e81ab27f3f5f86f38fec1b92f81e5f47582c77a7
Yota Adachi <yota961219@gmail.com>
2024-02-17 15:45:03 +0900

    10日目

--- 10日目/subquery1.sql
+++ 10日目/subquery1.sql
@@ -1,66 +1,82 @@
-SHOW DATABASES;
-
-USE day_10_14_db;
-
 SHOW TABLES;
 
 SELECT * FROM employees;
 
+DESCRIBE employees;
+
 -- 部署一覧
 SELECT * FROM departments;
 
--- INで絞り込む
-SELECT * FROM employees WHERE department_id IN(1,2);
+-- 副問い合わせ
+SELECT * 
+FROM employees
+WHERE department_id 
+IN(SELECT id
+FROM departments
+WHERE name
+IN("経営企画部", "営業部"));
 
--- 副問い合わせを使う
-SELECT * FROM departments WHERE name IN ("経営企画部", "営業部");
-
-SELECT * FROM employees WHERE department_id IN 
-(SELECT id FROM departments WHERE name IN ("経営企画部", "営業部"));
+SELECT * FROM students;
+SELECT * FROM users;
 
 -- 複数カラムのIN（副問い合わせ）
 SELECT * FROM students
-WHERE (first_name, last_name) IN
-(SELECT first_name, last_name FROM users);
+WHERE (first_name, last_name) IN (
+  SELECT first_name, last_name FROM users
+ );
 
--- 副問い合わせ：集計を使う
-SELECT MAX(age) FROM employees;
+-- 集計関数（副問い合わせ）
+DESCRIBE employees;
 
 SELECT * FROM employees
-WHERE age < (SELECT AVG(age) FROM employees);
+WHERE age = (
+ SELECT MAX(age) FROM employees
+);
 
--- 副問い合わせ：FROMで用いる
-SELECT
-MAX(avg_age) AS "部署ごとの平均年齢の最大",
-MIN(avg_age) AS "部署ごとの平均年齢の最小"
-FROM
-(SELECT department_id, AVG(age) AS avg_age FROM employees GROUP BY department_id) AS tmp_emp;
+SELECT * FROM employees
+WHERE age < (
+ SELECT AVG(age) FROM employees
+);
 
--- 年代の集計
-SELECT
- MAX(age_count), MIN(age_count)
-FROM
-(SELECT FLOOR(age/10)*10, COUNT(*) AS age_count FROM employees GROUP BY FLOOR(age/10)) AS age_summary;
+SHOW TABLES;
 
--- 副問い合わせ：SELECTの中に書く
-SELECT * FROM customers;
-SELECT * FROM orders;
+-- FROM句で使用する（副問い合わせ）
+SELECT MAX(tmp_emp.avg_age) AS "部署ごとの平均年齢の最大"
+FROM(
+ SELECT department_id, AVG(age) AS avg_age
+ FROM employees GROUP BY department_id
+) AS tmp_emp;
+
+DESCRIBE employees;
+
+-- 年代の集計（副問い合わせ）
+SELECT MAX(tmp_emp.age_count),
+ MIN(tmp_emp.age_count)
+FROM(
+ SELECT FLOOR(age/10), COUNT(*) AS age_count FROM employees
+ GROUP BY FLOOR(age/10)
+) AS tmp_emp;
+
+
+-- SELECTの中で使用（副問い合わせ）
+DESCRIBE orders;
+DESCRIBE customers;
 
 SELECT
  cs.id,
  cs.first_name,
  cs.last_name,
-(
-  SELECT MAX(order_date) FROM orders AS order_max WHERE cs.id = order_max.customer_id
-) AS "最新の注文",
-(
-  SELECT MIN(order_date) FROM orders AS order_max WHERE cs.id = order_max.customer_id
-) AS "最古の注文",
-(
-  SELECT SUM(order_amount * order_price) FROM orders AS order_max WHERE cs.id = order_max.customer_id
-) AS "全支払い金額"
-FROM 
+ (
+  SELECT MAX(order_date)
+  FROM orders AS order_max 
+  WHERE cs.id = order_max.customer_id
+ ) AS "直近の注文日",
+ (
+  SELECT SUM(order_amount * order_price)
+  FROM orders AS tmp_order
+  WHERE cs.id = tmp_order.customer_id
+ ) AS "全支払金額"
+FROM
  customers AS cs
 WHERE
  cs.id < 10;
- 
