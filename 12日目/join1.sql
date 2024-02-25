1a333daa6d0dc1fad3f7fb3f667b177b361583b7
Yota Adachi <yota961219@gmail.com>
2024-02-25 14:24:48 +0900

    12日目

--- 12日目/join1.sql
+++ 12日目/join1.sql
@@ -14,17 +14,26 @@ INNER JOIN departments AS dt
 ON emp.department_id = dt.id;
 
 -- 特定のカラムを取り出す
-SELECT emp.id, emp.first_name, emp.last_name, dt.id AS department_id, dt.name AS department_name
+SELECT emp.id,
+ 	   emp.first_name,
+ 	   emp.last_name ,
+ 	   emp.age,
+ 	   emp.manager_id,
+ 	   dt.id AS department_id,
+ 	   dt.name
 FROM employees AS emp
-INNER JOIN departments AS dt 
+INNER JOIN departments AS dt
 ON emp.department_id = dt.id;
 
 -- 複数のレコードで紐づける
 SELECT * FROM students AS std
-INNER JOIN users AS usr
-ON std.first_name = usr.first_name AND std.last_name = usr.last_name;
+INNER JOIN
+users AS usr
+ON std.first_name = usr.first_name 
+AND std.last_name = usr.last_name;
 
 -- =以外で紐づける
 SELECT * FROM employees AS emp
-INNER JOIN students AS std
+INNER JOIN
+ students AS std
 ON emp.id < std.id;
