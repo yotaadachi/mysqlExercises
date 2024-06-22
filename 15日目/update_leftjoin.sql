2a1f37e66062f46c4622a74a2d2e2a7be984d49e
Yota Adachi <yota961219@gmail.com>
2024-06-22 17:27:06 +0900

    15日目

--- 15日目/update_leftjoin.sql
+++ 15日目/update_leftjoin.sql
@@ -12,3 +12,35 @@ UPDATE employees AS emp
 LEFT JOIN departments AS dt
 ON emp.department_id = dt.id
 SET emp.department_name = COALESCE(dt.name, "不明");
+
+-- WITHを使ったUPDATE
+SELECT * FROM stores;
+
+ALTER TABLE stores ADD all_sales INT;
+
+SELECT * FROM items;
+
+SELECT * FROM orders;
+
+WITH tmp_sales AS(
+  SELECT ite.store_id,
+    SUM(ord.item_id * ord.order_amount) AS total_sales
+  FROM items ite
+  INNER JOIN orders ord
+  ON ite.id = ord.item_id
+  GROUP BY ite.store_id
+)
+UPDATE stores st
+INNER JOIN tmp_sales ts
+ON st.id = ts.store_id
+SET st.all_sales = ts.total_sales;
+
+SELECT * FROM stores;
+
+
+-- DELETE
+
+SELECT * FROM employees
+WHERE department_id = (
+ SELECT id FROM departments WHERE name = "開発部"
+)
